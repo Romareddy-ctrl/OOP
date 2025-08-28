c8b4b1a489e5bcc63772e379e0c6fc1729c66f4a
Roma <romareddy6565@gmail.com>
2025-08-28 09:42:04 +0530

    Add files via upload

---
+++ Class,Object in OOP.py
@@ -0,0 +1,60 @@
+#OOP
+#Create a Class
+class MyClass:
+  x = 5
+
+
+#Create a object
+p1 = MyClass()
+print(p1.x)
+
+#The __init__() Method
+class Person:
+  def __init__(self, name, age):
+    self.name = name
+    self.age = age
+
+p1 = Person("John", 36)
+
+print(p1.name)
+print(p1.age)
+
+
+#without __str__() Method
+class Person:
+  def __init__(self, name, age):
+    self.name = name
+    self.age = age
+
+p1 = Person("John", 36)
+
+print(p1)
+
+
+#The __str__() Method
+class Person:
+  def __init__(self, name, age):
+    self.name = name
+    self.age = age
+
+p1 = Person("John", 36)
+
+print(p1)
+
+#The Self Parameter
+class Person:
+  def __init__(mysillyobject, name, age):
+    mysillyobject.name = name
+    mysillyobject.age = age
+
+  def myfunc(abc):
+    print("Hello my name is " + abc.name)
+
+p1 = Person("John", 36)
+p1.myfunc()
+
+
+
+
+
+  
